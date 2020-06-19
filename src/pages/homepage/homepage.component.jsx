0f3828e64cac5dade4704ac160b42fa961d39dc6
Prathamesh Borgharkar <35142003+Prathameshsb21995@users.noreply.github.com>
2020-06-19 02:50:00 -0400

    separated the code into its component and pages folder and the main container is completed

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../component/directory/directory.component';
+import './homepage.style.scss';
+
+
+export const Homepage = props => (
+    <div className = "homepage">
+        <Directory/>
+    </div>
+)
+ 
