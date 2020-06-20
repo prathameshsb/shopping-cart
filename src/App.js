a4f1d7f1a1f7a18d7fd88c0a75380d2f72bb88b6
Prathamesh Borgharkar <35142003+Prathameshsb21995@users.noreply.github.com>
2020-06-20 14:02:28 -0400

    shop page, with all categories along with their css

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import { Homepage } from './pages/homepage/homepage.component';
-
-const Hatspage = () => (
-  <div>
-    <h1>Hats Pages</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App"> 
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={Hatspage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
 }
-
 
 export default App;
