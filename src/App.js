247611c17c059fda2ebd81cbfe1daaa9eb3b2488
Prathamesh Borgharkar <35142003+Prathameshsb21995@users.noreply.github.com>
2020-06-20 15:55:05 -0400

    Added the header menu with working link for logo and shop

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import { Homepage } from './pages/homepage/homepage.component';
+import Header from "./component/header/header.component";
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App"> 
+    <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
