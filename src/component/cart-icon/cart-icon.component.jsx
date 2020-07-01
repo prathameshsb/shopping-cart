d7c6cb80dadacb3799a8bdb37a664c8a472a25c9
Prathamesh Borgharkar <35142003+Prathameshsb21995@users.noreply.github.com>
2020-07-01 01:14:43 -0400

    cart-icon quantity number updates according to the number of items in the cart

--- src/component/cart-icon/cart-icon.component.jsx
+++ src/component/cart-icon/cart-icon.component.jsx
@@ -4,10 +4,10 @@ import {toggleCartHidden} from '../../redux/cart/cart.action';
 import {ReactComponent as ShoppingIcon} from '../../assests/shopping-bag.svg.svg';
 import './cart-icon.style.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
@@ -15,4 +15,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemCount:  cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
