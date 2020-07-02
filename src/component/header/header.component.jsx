f0e871f3dfa5155f5617eb016c9877127bb20375
Prathamesh Borgharkar <35142003+Prathameshsb21995@users.noreply.github.com>
2020-07-02 12:39:39 -0400

    added cart selector and user selector and added background-image to the app

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import {createStructuredSelector} from 'reselect';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import {selectCartHidden} from '../../redux/cart/cart.selector';
+import {selectCurrentUser} from '../../redux/user/user.selector';
 
 import { ReactComponent as Logo } from '../../assests/crown.svg.svg';
 
@@ -40,9 +43,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
